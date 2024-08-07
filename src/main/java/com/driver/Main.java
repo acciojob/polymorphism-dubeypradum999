f9431fd5c2c12175b6d86501a36e372511042946
Pradumn Dubey <baba.pradumn.dubey@gmail.com>
2024-08-07 17:01:43 +0530

    Modifying Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,32 +1,24 @@
 package com.driver;
 
 public class Main {
-
-
-    public static class product{
-
-        public int product(int x , iny y){
-            return x*y;
+    public static class Product {
+        public int product(int x, int y) {
+            return x * y;
+        }
+        public int product(int x, int y, int z) {
+            return x * y * z;
         }
 
-        public int product(int x, int y, int z){
-            return x*y*z;
+        public double product(double x, double y) {
+            return x * y;
         }
-
-        public double product(double x, double y){
-            return x*y;
-        }
-
-
-
     }
-   public static void main(String args[]){
 
+    public static void main(String[] args) {
         Product p = new Product();
-        p.product(x:2 , y:3);
-        p.product(x:2, y:6, z:8);
-        p.product(x:3.0 , y:5.0);
 
-   }
-
+        System.out.println("Product of 2 and 3: " + p.product(2, 3));              // Output: 6
+        System.out.println("Product of 2, 3, and 4: " + p.product(2, 3, 4));       // Output: 24
+        System.out.println("Product of 2.5 and 3.5: " + p.product(2.5, 3.5));      // Output: 8.75
+    }
 }
